2a2f4d275d4be4f3b40f0ad142ad6ab017c411d9
Ryan Hu <ryanhuuu@gmail.com>
2018-03-26 23:40:55 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,9 @@ def reformat_languages(languages)
   
   languages.each do |style, style_values|
     style_values.each do |language, language_values|
-
+      temp = {}
+      temp[:type] = language_values[:type]
+      temp[:style] = 
       result[language.to_sym] = {
                                     type: #{language_values[:type]},
                                     style: #{style}
