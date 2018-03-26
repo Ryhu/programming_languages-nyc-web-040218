210874d84010c1244072f6fbe54f6cf623212ea4
Ryan Hu <ryanhuuu@gmail.com>
2018-03-26 23:34:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,10 @@ def reformat_languages(languages)
   result = {}
   
   
-  languages.each do |key, val|
-    result[key.to_sym] = {}
+  languages.each do |style, style_values|
+    style_values.each do |language, type_hash|
+      
+      
     
   end
 end
